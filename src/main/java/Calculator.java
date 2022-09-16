a7e977eff610850d61a41f23024a7b4e2484025f
Roman TR <romanbukshak@mail.ru>
2022-09-17 00:15:29 +0700

    add test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,31 @@
 public class Calculator {
 
+    public static Integer result;
+    public static Integer sum (Integer value1, Integer value2) {
+        result = value1 + value2;
+        return result;
+    }
+
+    public static Integer difference (Integer value1, Integer value2) {
+        result = value1 - value2;
+        return result;
+    }
+
+    public static Integer multiplication (Integer value1, Integer value2) {
+        result = value1 * value2;
+        return result;
+    }
+
+    public static Integer division (Integer value1, Integer value2) {
+        result = value1 / value2;
+        return result;
+    }
+
+    public static void clear (){
+        result = 0;
+    }
+
+
+
+
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+
+    @BeforeEach
+    void clear(){
+        Calculator.clear();
+    }
+
+    @Test
+    public void testSum() {
+        Assertions.assertEquals((Integer)6,Calculator.sum(2,4));
+    }
+
+    @Test
+    void testDifference() {
+        Assertions.assertEquals((Integer)10, Calculator.difference(20,10));
+    }
+
+    @Test
+    void testMultiplication() {
+        Assertions.assertEquals((Integer)20,Calculator.multiplication(4,5));
+    }
+
+    @Test
+    void testDivision() {
+        Assertions.assertEquals((Integer)5, Calculator.division(20,4));
+    }
+}
