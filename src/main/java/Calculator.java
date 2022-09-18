573359078e174426fa0e6651c78cc30d267e6248
Roman Bukshak <romanbukshak@mail.ru>
2022-09-18 14:26:30 +0700

    Калькулятор через целые числа типа long

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,31 +1,29 @@
-import org.jetbrains.annotations.NotNull;
-import java.math.BigInteger;
-
 public class Calculator {
 
-    public static BigInteger result;
-    public static BigInteger sum (BigInteger value1, BigInteger value2) {
-        result = value1.add(value2);
+    private static long result;
+
+    public static long sum (long value1, long value2) {
+        result = value1 + value2;
         return result;
     }
 
-    public static BigInteger difference (BigInteger value1, BigInteger value2) {
-        result = value1.subtract(value2);
+    public static long difference (long value1, long value2) {
+        result = value1 - value2;
         return result;
     }
 
-    public static BigInteger multiplication (BigInteger value1, BigInteger value2) {
-        result = value1.multiply(value2);
+    public static long multiplication (long value1, long value2) {
+        result = value1 * value2;
         return result;
     }
 
-    public static BigInteger division (BigInteger value1, @NotNull BigInteger value2) {
-        result = value1.divide(value2);
+    public static long division (long value1, long value2) {
+        result = value1 / value2;
         return result;
     }
 
     public static void clear (){
-        result = BigInteger.ZERO;
+        result = 0;
     }
 
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,33 +1,32 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.math.BigInteger;
 
 public class CalculatorTest {
 
-
     @BeforeEach
-    void clear(){
+    public void clear(){
         Calculator.clear();
     }
 
     @Test
-    void testSum() {
-        Assertions.assertEquals(BigInteger.valueOf(22),Calculator.sum(BigInteger.valueOf(11),BigInteger.valueOf(11)));
+    public void testSum() {
+        Assertions.assertEquals(22,Calculator.sum(11,11), "Calculator.sum(11,11) fail!");
     }
 
     @Test
-    void testDifference() {
-        Assertions.assertEquals(BigInteger.valueOf(4), Calculator.difference(BigInteger.valueOf(6),BigInteger.valueOf(2)));
+    public void testDifference() {
+        Assertions.assertEquals(4, Calculator.difference(6,2), "Calculator.difference(6,2) fail!");
     }
 
     @Test
-    void testMultiplication() {
-        Assertions.assertEquals(BigInteger.valueOf(20),Calculator.multiplication(BigInteger.valueOf(4),BigInteger.valueOf(5)));
+    public void testMultiplication() {
+        Assertions.assertEquals(20,Calculator.multiplication(4,5), "Calculator.multiplication(4,5) fail!");
     }
 
     @Test
-    void testDivision() {
-        Assertions.assertEquals(BigInteger.valueOf(5), Calculator.division(BigInteger.valueOf(20),BigInteger.valueOf(4)));
+    public void testDivision() {
+        Assertions.assertEquals(5, Calculator.division(20,4), "Calculator.division(20,4) fail!");
     }
+
 }
